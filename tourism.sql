80aec84de6562e116ee7a0444ba4589156dc2ab9
MiaLPrice <43227891+MiaLPrice@users.noreply.github.com>
2019-11-15 15:50:35 -0500

    Create tourism.sql
    an update to schema_sql.sql
    added table inspections
    moved variables around

---
+++ tourism.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS itws6960_project;
+CREATE DATABASE itws6960_project;
+
+USE itws6960_project;
+DROP TABLE IF EXISTS wifi;
+DROP TABLE IF EXISTS restuarants;
+DROP TABLE IF EXISTS violations;
+DROP TABLE IF EXISTS landmarks;
+
+CREATE TABLE wifi {
+    longitude float(10),
+    latitude float(10),
+    wifiID varchar(50),
+    address varchar(255),
+    accessPointName varchar(255),
+    wifiType varchar(255)
+};
+
+CREATE TABLE restuarants {
+    CAMIS varchar(25),
+    longitude float(10),
+    latitude float(10),
+    restuarantName varchar(255),
+    street varchar(255),
+    zipcode int,
+    phone varchar() -- phone numebr char or int?? 
+};
+
+CREATE TABLE inspections {
+    CAMIS varchar(25),
+    InspectionDate timestamp,
+    violationCodes text[][],
+    score int,
+    grade char(1)
+};
+
+CREATE TABLE violations {
+    violationID varchar(25),
+    violationDescription varchar(255)
+};
+
+CREATE TABLE landmarks {
+    objectID int,
+    landmarkName varchar(255),
+    centerLogitude float(10),
+    centerLatitude float (10),
+    address varchar(255),
+    additionalInfoURL varchar(255) -- can be stored this way because database gives bit.ly that are all under 255 characters
+};
+
+CREATE TABLE subway {
+    longitude float(10),
+    latitude float(10),
+    name varchar(255),
+    lines varchar(25)
+};
