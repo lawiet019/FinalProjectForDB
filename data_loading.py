bbf1fb24f79474f40ce1c39b6a7858893ff2552c
MiaLPrice <43227891+MiaLPrice@users.noreply.github.com>
2019-11-17 21:42:37 -0500

    Start of data_loading.py, working on the tables: inspections, grades, restuarants, violations

--- data_loading.py
+++ data_loading.py
@@ -1,27 +1,104 @@
-#!/usr/bin/env python
-"""
-
-"""
 import psycopg2
-import pandas as pd
 from sodapy import Socrata
-
-conn = psycopg2.connect("host=localhost dbname=itws6960_project user=postgres")
-client = Socrata("data.cityofnewyork.us", None)
-
+import pandas as pd
 
 RESTAURANT_DATASET_IDENTIFIER = "43nn-pn8j"
-WIFI_DATASET_IDENTIFIER = "yjub-udmw"
-SUBWAY_DATASET_IDENTIFIER = "i9wp-a4ja"
-LANDMARK_DATASET_IDENTIFIER = "7mgd-s57w"
-# IMPORT CSVs for SUBWAY DATASET AND LANDMARK DATASET INSTEAD, BECUASE NEITHER IDENIFIER WANTS TO WORK 
+'''
+    table                 done
+    inspections               
+    grades                  
+    restuarants             
+    violations              
+'''
 
-results = client.get(RESTAURANT_DATASET_IDENTIFIER, limit = 400000)
+client = Socrata("data.cityofnewyork.us", None)
+results = client.get(RESTAURANT_DATASET_IDENTIFIER, limit = 10)
+full_restuarant_df = pd.DataFrame.from_records(results)
 
-results_df = pd.DataFrame.from_records(results)
+try:
+    conn = psycopg2.connect("dbname = 'itws6960_project' user = 'postgres' host = 'localhost'")
+except psycopg2.DatabaseError:
+    print('I am unable to connect the database')
+    sys.exit(1)
 
-print(results_df)
+cursor = conn.cursor()
+
+data =  {'CAMIS': list(full_restuarant_df['camis']), 
+        'inspectionDate': list(full_restuarant_df['inspection_date']),
+        'violationCodes': list(full_restuarant_df['violation_code']),
+        'score': list(full_restuarant_df['score'])
+        }
+inspections_df = pd.DataFrame(data)
+'''
+for i in range(0, len(inspections_df)):
+    cursor.execute( "INSERT INTO inspections VALUES (%s, %s, %s, %s)" % 
+                    (inspections_df['CAMIS'][i], 
+                    inspections_df['inspectionDate'][i],
+                    inspections_df['violationCodes'][i],
+                    inspections_df['score'][i]
+                    )
+        )
+'''
+data =  {'CAMIS': list(full_restuarant_df['camis']), 
+        'gradeDate': list(full_restuarant_df['grade_date']),
+        'grade': list(full_restuarant_df['grade'])
+        }
+grades_df = pd.DataFrame(data)
+'''
+for i in range(0, len(grades_df)):
+    cursor.execute( "INSERT INTO grades VALUES (%s, %s, %s)" % 
+                    (grades_df['CAMIS'][i], 
+                    grades_df['gradeDate'][i],
+                    grades_df['grade'][i]
+                    )
+        )
+''' 
+
+data =  {'CAMIS': list(full_restuarant_df['camis']),
+        'longitude': list(full_restuarant_df['longitude']),
+        'latitude': list(full_restuarant_df['latitude']),
+        'restuarantName': list(full_restuarant_df['dba']),
+        'cuisineDescription': list(full_restuarant_df['cuisine_description']),
+        'street': list(full_restuarant_df['street']),
+        'zipcode': list(full_restuarant_df['zipcode']),
+        'phone': list(full_restuarant_df['phone'])
+        }
+restuarants_df = pd.DataFrame(data)
+'''
+for i in range(0, len(restuarants_df)):
+    cursor.execute( "INSERT INTO restuarants VALUES (%s, %s, %s,%s, %s, %s,%s, %s)" % 
+                    (restuarants_df['CAMIS'][i], 
+                    restuarants_df['longitude'][i],
+                    restuarants_df['latitude'][i],
+                    restuarants_df['restuarantName'][i],
+                    restuarants_df['cuisineDescription'][i],
+                    restuarants_df['street'][i],
+                    restuarants_df['zipcode'][i],
+                    restuarants_df['phone'][i]
+                    )
+        )
+''' 
+data =  {'violationCode': full_restuarant_df['violation_code'],
+        'violationDescription': full_restuarant_df['violation_description']
+        }
+violations_df = pd.DataFrame(data)
 
 
-if __name__ == '__main__':    #code to execute if called from command-line
-    pass    #do nothing - code deleted
+violations_dict = {}
+for i in range(0, len(violations_df)):
+    code = violations_df['violationCode'][i]
+    if code not in violations_dict:
+        violations_dict[code] = violations_df['violationDescription'][i]
+
+violations_df = pd.DataFrame(list(violations_dict.items()), columns=list(violations_df.columns) )
+'''
+for i in range(0, len(violations_df)):
+    cursor.execute( "INSERT INTO grades VALUES (%s, %s)" % 
+                    (violations_df['violationCode'][i], 
+                    violations_df['violationDescription'][i]
+                    )
+        )
+''' 
+conn.commit() 
+# violation_code
+# violation_description
